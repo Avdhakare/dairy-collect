f0a8d81668007128f287bbf9eab9a887e96d0f08
v_avdhesh.dhakare <v_avdhesh.dhakare@carboceramics.com>
2024-10-02 10:05:48 +0530

    changes of  buttom of camera and prifile

--- Src/Common/Button.tsx
+++ Src/Common/Button.tsx
@@ -4,6 +4,7 @@ import { BUTTON } from "../Constant"
 const Button=({title=undefined,disabled=false,onPress}:BUTTON)=>{
     return(
         <TouchableOpacity disabled={disabled} onPressOut={onPress} className="bg-sky-500 self-center py-2 px-8 rounded-lg">
+            
             <Text className="text-center text-white text-base font-bold">
                 {title?title:"Submit"}
             </Text>
